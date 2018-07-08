aab967813da0856f8d94abb5e3ad7ca4a1428d5b
root <root@localhost.localdomain>
2018-07-07 22:51:47 -0700

    Link_Hash insert find remove

--- Link_hash/Link_hash.c
+++ Link_hash/Link_hash.c
@@ -66,11 +66,63 @@ void Hashinsert(Hashtable *hash,Typekey key,Typeval value)
             cur=cur->next;
         }
         HashNode *newnode=CreateHashNode(key,value);
-        newnode->next=hash->data[offset];
+        newnode->next=hash->data[offset];//不带头结点的头插，新节点指向头结点，
         hash->data[offset]=newnode;
         break ;
     }
     hash->size++;
+}
+void HashFind(Hashtable *hash,Typekey key,Typeval *value)
+{
+    //判断哈希表是否为空，为空直接返回
+    //根据key值计算出哈希表的位置，位置为空直接返回，位置不为空，遍历
+    if(hash==NULL)
+        return ;
+    int offset;
+    offset=hash->func(key);
+    HashNode* cur=hash->data[offset];
+        while(cur!=NULL)
+        {
+            if(cur->key==key)
+            {
+                *value = hash->data[offset]->val;
+                break;
+            }  
+            cur=cur->next;
+        }
+}
+void Hashremove(Hashtable *hash,Typekey key)
+{
+    //根据key计算出哈希表中的位置
+    //遍历链表，如果找到key，保存它前一个元素，跳出循环，没找到，直接返回
+    //如果头结点为key，头删，否则直接删
+    if(hash==NULL)
+        return;
+    int offset;
+    offset=hash->func(key);
+    HashNode* cur=hash->data[offset];
+    HashNode* pre=NULL;
+    while(cur!=NULL)
+    {
+        if(cur->key==key)
+            break;
+        pre=cur;
+        cur=cur->next;
+    }
+    if(cur==NULL)
+        return;
+    //走到这key值一定存在，找到了key
+    if(pre==NULL)
+    {
+        HashNode *tmp=hash->data[offset];
+        hash->data[offset]=tmp->next;
+        free(tmp);
+    }
+    else
+    {
+        pre->next=cur->next;
+        free(cur);
+    }
 }
 int main()
 {
@@ -84,6 +136,10 @@ int main()
     Hashinsert(&hash,900,900);
     Hashinsert(&hash,460,460);
     Hashinsert(&hash,443,443);
-    printf("%d %d",((&hash)->data[1])->key,((&hash)->data[1])->next->key);
+    //printf("%d %d",((&hash)->data[1])->key,((&hash)->data[1])->next->key);
+    int a;
+    //Hashremove(&hash,443);
+    HashFind(&hash,443,&a);
+    printf("%d",a);
     return 0;
 }
